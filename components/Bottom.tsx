ed0d6c33fa56813ec9c6a14edc6845739300f9d8
Ezra Chai <juanzhe2@gmail.com>
2022-08-30 00:40:50 +0800

    changed a bit

--- components/Bottom.tsx
+++ components/Bottom.tsx
@@ -1,5 +1,5 @@
 export default function Bottom(){
     return (
-        <div className=" w-screen h-32 from-slate-50 via-slate-50  bg-gradient-to-b to-gray-300"/>
+        <div className=" min-w-full h-32 from-slate-50 via-slate-50  bg-gradient-to-b to-slate-200"/>
     )
 }
